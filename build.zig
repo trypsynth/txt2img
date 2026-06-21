f23d319117099a30e247080b2068977f42e42018
Quin Gillespie <trypsynth@gmail.com>
2026-06-21 13:55:05 -0600

    Upgrade zignal and build script to work with 0.17.0-dev.931+84f84267c

--- build.zig
+++ build.zig
@@ -17,9 +17,7 @@ pub fn build(b: *std.Build) void {
 	b.installArtifact(exe);
 	const run_cmd = b.addRunArtifact(exe);
 	run_cmd.step.dependOn(b.getInstallStep());
-	if (b.args) |args| {
-		run_cmd.addArgs(args);
-	}
+	run_cmd.addPassthruArgs();
 	const run_step = b.step("run", "Build and run txt2img");
 	run_step.dependOn(&run_cmd.step);
 }
